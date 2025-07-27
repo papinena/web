e18e1cb2bed39f8f63424c01422bc8446ff6d123
euliveiras <matheus.skm@hotmail.com>
2025-07-27 14:42:55 -0300

    refactor: admin dash layout

--- app/routes/admin-dashboard.tsx
+++ app/routes/admin-dashboard.tsx
@@ -8,7 +8,7 @@ import { cn } from "~/lib/utils";
 
 function Widget({ children }: { children: ReactNode }) {
   return (
-    <Box className="border-2 rounded-xl gap-2 px-2 pb-5 pt-2 min-h-28 flex-col  border-green-primary">
+    <Box className="border-2 rounded-xl gap-2 px-2 pb-5 pt-2 min-h-28 flex-col border-green-primary">
       {children}
     </Box>
   );
@@ -43,45 +43,48 @@ export default function Dashboard() {
   const iconClassName = "size-9";
 
   return (
-    <Box className="flex-col gap-5">
-      <Widget>
-        <WidgetTitle className="font-bold">
-          Painel de controle da Administração
-        </WidgetTitle>
-        <Text>
-          Olá, <strong className="text-blue-primary">@{employee?.name}</strong>,
-          por aqui você pode acessar todas as informações relacionadas a conta
-          do condomínio!
-        </Text>
-      </Widget>
-      <Widget>
-        <WidgetTitle
-          className=""
-          icon={<Image src="/image 113.svg" className={iconClassName} />}
-        >
-          Cadastro da Administração
-        </WidgetTitle>
-        <Text>
-          Edite, remova ou inclua novas informações sobre a administração do
-          condomínio
-        </Text>
-      </Widget>
-      <Widget>
-        <WidgetTitle
-          icon={<Image src="/image 113.svg" className={iconClassName} />}
-        >
-          Gerenciar moradores
-        </WidgetTitle>
-        <Text>Aprove ou remova os moradores do seu condomínio</Text>
-      </Widget>
-      <Widget>
-        <WidgetTitle
-          icon={<Image src="/image 112.svg" className={iconClassName} />}
-        >
-          Postagens do Condomínio
-        </WidgetTitle>
-        <Text>Visualize, altere ou remova as postagens.</Text>
-      </Widget>
+    <Box className="flex-1">
+      <Box className="flex-col gap-5">
+        <Widget>
+          <WidgetTitle className="font-bold">
+            Painel de controle da Administração
+          </WidgetTitle>
+          <Text>
+            Olá,
+            <strong className="text-blue-primary">@{employee?.name}</strong>,
+            por aqui você pode acessar todas as informações relacionadas a conta
+            do condomínio!
+          </Text>
+        </Widget>
+        <Widget>
+          <WidgetTitle
+            className=""
+            icon={<Image src="/image 113.svg" className={iconClassName} />}
+          >
+            Cadastro da Administração
+          </WidgetTitle>
+          <Text>
+            Edite, remova ou inclua novas informações sobre a administração do
+            condomínio
+          </Text>
+        </Widget>
+        <Widget>
+          <WidgetTitle
+            icon={<Image src="/image 113.svg" className={iconClassName} />}
+          >
+            Gerenciar moradores
+          </WidgetTitle>
+          <Text>Aprove ou remova os moradores do seu condomínio</Text>
+        </Widget>
+        <Widget>
+          <WidgetTitle
+            icon={<Image src="/image 112.svg" className={iconClassName} />}
+          >
+            Postagens do Condomínio
+          </WidgetTitle>
+          <Text>Visualize, altere ou remova as postagens.</Text>
+        </Widget>
+      </Box>
     </Box>
   );
 }
