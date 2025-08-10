443a05678ccee73e9b2e8031ce1c7e376e37a549
euliveiras <matheus.skm@hotmail.com>
2025-08-10 02:30:56 -0300

    fix: mapper import

--- app/services/create-user.ts
+++ app/services/create-user.ts
@@ -1,12 +1,12 @@
 import { apiRequest, api } from "~/utils/api";
 import type { CreateUserSchema } from "~/parsers/create-user";
 import type { z } from "zod";
-import { userMapper } from "~/mappers/user";
+import { UserMapper } from "~/mappers/user";
 
 export async function createUser(data: z.infer<typeof CreateUserSchema>) {
   const { BASE_URL } = api();
   const body = JSON.stringify({
-    user: userMapper.toAPI(data),
+    user: UserMapper.toAPI(data),
     tags: data.tags?.map((t) => ({
       label: t,
     })),
