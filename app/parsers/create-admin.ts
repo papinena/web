97cf712b1166279f64f81bbe08ad3e2dbc69bdda
euliveiras <matheus.skm@hotmail.com>
2025-10-13 10:04:41 -0300

    fix: bug in CreateAdminSchema birth date

--- app/parsers/create-admin.ts
+++ app/parsers/create-admin.ts
@@ -1,61 +1,52 @@
 import z from "zod";
+import { DateFormatter } from "~/utils/date-formatter";
 
 export const CreateAdminSchema = z
   .object({
-    employee: z
-      .object({
-        name: z.string().min(1, "Mínimo de 4 caracteres"),
-        lastName: z.string().min(4, "Mínimo de 4 caracteres"),
-        birthDate: z.string().min(1, "Data de nascimento é obrigatória"),
-        photo: z.string().optional(),
-        telephone: z
-          .string()
-          .regex(
-            /^\(\d{2}\) \d{5}-\d{4}$/,
-            "O telefone deve estar no formato (XX) XXXXX-XXXX"
-          ),
-        position: z.string().min(2, { error: "Mínimo de 2 caracteres" }),
-        isResident: z.boolean({ error: "Obrigatório" }),
-        block: z.string().optional(),
-        apartment: z.string().optional(),
-        email: z
-          .email({ error: "Email inválido" })
-          .transform((val) => val.toLowerCase()),
-        confirmEmail: z
-          .email({ error: "Email inválido" })
-          .transform((val) => val.toLowerCase()),
-        password: z
-          .string()
-          .min(8, { error: "A senha deve ter no mínimo 8 caracteres" })
-          .regex(/\d/, { error: "A senha deve conter ao menos um número" })
-          .regex(/[^a-zA-Z0-9]/, {
-            error: "A senha deve conter ao menos um caractere especial",
-          }),
-        confirmPassword: z
-          .string()
-          .min(8, { error: "A senha deve ter no mínimo 8 caracteres" })
-          .regex(/\d/, { error: "A senha deve conter ao menos um número" })
-          .regex(/[^a-zA-Z0-9]/, {
-            error: "A senha deve conter ao menos um caractere especial",
-          }),
-      })
-
-      .refine(
-        (data) => {
-          const birthDate = new Date(data.birthDate);
-          const today = new Date();
-          let age = today.getFullYear() - birthDate.getFullYear();
-          const m = today.getMonth() - birthDate.getMonth();
-          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-          }
+    employee: z.object({
+      name: z.string().min(1, "Mínimo de 4 caracteres"),
+      lastName: z.string().min(4, "Mínimo de 4 caracteres"),
+      birthDate: z
+        .string()
+        .min(1, "Data de nascimento é obrigatória")
+        .refine((data) => {
+          const dobDate = DateFormatter.parse(data, "dd/MM/yyyy");
+          const age = DateFormatter.differenceInYears(new Date(), dobDate);
           return age >= 18;
-        },
-        {
-          message: "Você deve ter no mínimo 18 anos",
-          path: ["birthDate"],
-        }
-      ),
+        }, "Você deve ter no mínimo 18 anos"),
+      photo: z.string().optional(),
+      telephone: z
+        .string()
+        .regex(
+          /^\(\d{2}\) \d{5}-\d{4}$/,
+          "O telefone deve estar no formato (XX) XXXXX-XXXX"
+        ),
+      position: z.string().min(2, { error: "Mínimo de 2 caracteres" }),
+      isResident: z.boolean({ error: "Obrigatório" }),
+      block: z.string().optional(),
+      apartment: z.string().optional(),
+      email: z
+        .email({ error: "Email inválido" })
+        .transform((val) => val.toLowerCase()),
+      confirmEmail: z
+        .email({ error: "Email inválido" })
+        .transform((val) => val.toLowerCase()),
+      password: z
+        .string()
+        .min(8, { error: "A senha deve ter no mínimo 8 caracteres" })
+        .regex(/\d/, { error: "A senha deve conter ao menos um número" })
+        .regex(/[^a-zA-Z0-9]/, {
+          error: "A senha deve conter ao menos um caractere especial",
+        }),
+      confirmPassword: z
+        .string()
+        .min(8, { error: "A senha deve ter no mínimo 8 caracteres" })
+        .regex(/\d/, { error: "A senha deve conter ao menos um número" })
+        .regex(/[^a-zA-Z0-9]/, {
+          error: "A senha deve conter ao menos um caractere especial",
+        }),
+    }),
+
     condominium: z.object({
       name: z.string().min(2, { error: "Mínimo de 2 caracteres" }),
       usefulInformation: z.string().optional(),
