a991c123ce56d5bc59075409f759559a61ef07b3
euliveiras <matheus.skm@hotmail.com>
2025-10-16 10:00:10 -0300

    refactor: changed native bridge payload

--- app/utils/send-user-id-to-native.ts
+++ app/utils/send-user-id-to-native.ts
@@ -1,5 +1,8 @@
+import { api } from "./api";
 import { authUtils } from "./auth";
 import * as Sentry from "@sentry/react";
+
+const apiUrl = api().BASE_URL;
 
 export function sendUserIdToNative() {
   try {
@@ -10,10 +13,24 @@ export function sendUserIdToNative() {
       window.webkit.messageHandlers.authHeaderHandler
     ) {
       const authHeader = authUtils.getAuthHeader();
+
+      // Ensure both are available before sending
+      if (!authHeader || !apiUrl) {
+        console.warn(
+          "Missing Auth Header or API URL. Cannot send config to native."
+        );
+        return;
+      }
+
+      const payload = {
+        authHeader,
+        apiUrl,
+      };
+
       if (authHeader) {
-        console.log("Sending auth header to native:", authHeader);
+        console.log("Sending payload to native:", payload);
         // The name 'authHeaderHandler' must match what you set in Swift
-        window.webkit.messageHandlers.authHeaderHandler.postMessage(authHeader);
+        window.webkit.messageHandlers.authHeaderHandler.postMessage(payload);
       } else {
         console.warn("User ID not found in local storage.");
       }
